1341fd6ef95e12d16a5181fb9cb04fabfab923fe
Harry177 <harry177@rambler.ru>
2022-09-16 04:57:25 +0200

    feat: create an example of simple react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app__wrapper'>
+      <header className='header'>
+        <img src='https://cdn.logo.com/hotlink-ok/logo-social.png' alt='logo'></img>
       </header>
+      <nav className='nav'>
+        <div>Profile</div>
+        <div>Messages</div>
+        <div>News</div>
+        <div>Music</div>
+        <div>Settings</div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://www.ghimprove.com/_blog/images/posts/hello-world/mountain.jpg' alt='banner' class='banner'></img>
+        </div>
+        <div>
+          avatar + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New posts
+          </div>
+          <div>
+            <div>
+              Post 1
+            </div>
+            <div>
+              Post 2
+            </div>
+          </div>
+        </div>
+      </div>
+  
     </div>
   );
 }
