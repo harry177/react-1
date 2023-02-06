62493f510a4ff01b6db4d0baa886c20c7493a6f6
Harry177 <harry177@rambler.ru>
2023-02-06 23:21:49 +0100

    feat: create examples of inline styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import classes from "./Navbar.module.css";
 
+const customColor = "blue";
+const myObj = { color: customColor, fontFamily: "Georgia" };
+
 const Navbar = () => {
   return (
     <nav className={classes.nav}>
       <div className={classes.item}>Profile</div>
       <div className={`${classes.item} ${classes.active}`}>Messages</div>
       <div className={classes.item}>News</div>
-      <div className={classes.item}>Music</div>
+      <div style={myObj}>Music</div>
       <div className={classes.item}>Settings</div>
     </nav>
   );
