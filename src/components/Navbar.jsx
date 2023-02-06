d0197abd16cbd2975e50fb3239e973b30fe7b954
Harry177 <harry177@rambler.ru>
2023-02-06 19:26:04 +0100

    refactor: change one css-style on composed-of-two style using React module approach

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className={classes.nav}>
       <div className={classes.item}>Profile</div>
-      <div className={classes.item}>Messages</div>
+      <div className={`${classes.item} ${classes.active}`}>Messages</div>
       <div className={classes.item}>News</div>
       <div className={classes.item}>Music</div>
       <div className={classes.item}>Settings</div>
